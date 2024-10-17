54b6853d65886141f3968ece3e5086185a5f26b0
Miguel Angel <m.hornos@sapalomera.cat>
2024-10-17 17:01:18 +0200

    css modificat

--- Index.php
+++ Index.php
@@ -1,20 +1,20 @@
 <!-- Miguel Ángel Hornos -->
 
-<!-- pagina principal de la web -->
-<?php
-include "Vistes/navbar.php";
-include "Model/mostrarLlista.php";
-?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pr4</title>
-
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="Estils\estils.css">
     </head>
     <body>
+        <h1>Mecànic Admin de Garatges</h1>
     </body>
 </html>
 
+<?php
+include "Vistes/navbar.php";
+include "Model/mostrarLlista.php";
+?>
